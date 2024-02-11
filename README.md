2f414d3f79ddcef1c6db8750eb326a47c03bc812
Daniel Portela <danielportela.ti@gmail.com>
2024-02-11 18:08:28 -0300

    novos recursos e melhorias para o projeto

---
+++ README.md
@@ -0,0 +1,76 @@
+<div align="center">
+
+Apresentação: Projeto Cafeteria Toque Sedutor - Freelancer
+
+![HTML](https://img.shields.io/badge/-HTML-0D1117?style=for-the-badge&logo=html5&labelColor=0D1117)&nbsp;
+![CSS](https://img.shields.io/badge/-CSS-0D1117?style=for-the-badge&logo=CSS3&logoColor=blue&labelColor=0D1117)&nbsp;
+![JavaScript](https://img.shields.io/badge/-javascript-0D1117?style=for-the-badge&logo=javascript&logoColor=yellow&labelColor=0D1117)&nbsp;
+
+<p>Hoje estou empolgado em apresentar o meu novo projeto, a Cafeteria Toque Sedutor, uma plataforma web desenvolvida com HTML, CSS e JavaScript. Vamos explorar as características e elementos que tornam esta cafeteria virtual única e cativante.</p>
+
+<a href="https://danielportela.netlify.app/"><strong>Demonstração ao vivo</strong></a>
+</div>
+
+![alt text](preview-desktop.jpg)
+
+![alt text](preview-responsive.jpg)
+
+
+<b>Objetivo do projeto:</b>
+
+A Cafeteria Toque Sedutor foi concebida com a missão de oferecer uma experiência online envolvente e deliciosa, proporcionando aos clientes uma amostra do ambiente acolhedor e dos produtos exclusivos oferecidos pela cafeteria.
+
+<b>Tecnologias utilizadas:</b>
+
+HTML5: 
+
+Utilizado para estruturar o conteúdo da página web, garantindo uma organização lógica e semântica.
+
+CSS3: 
+
+Responsável pela estilização e apresentação visual da plataforma, garantindo uma experiência estética agradável e coerente.
+
+JavaScript: 
+
+Adicionado para tornar a plataforma interativa e dinâmica, permitindo funcionalidades como carrossel de imagens, navegação suave e interações do usuário.
+
+Navegação intuitiva: 
+
+Implementei uma barra de navegação que permite aos usuários acessarem facilmente diferentes seções da plataforma, como Início, Sobre, Cardápio e Avaliações.
+
+Apresentação visual atraente: 
+
+Utilizei imagens de alta qualidade e elementos visuais cativantes para destacar os produtos e criar uma atmosfera convidativa.
+
+Cardápio interativo: 
+
+Apresentei uma variedade de itens do cardápio com descrições detalhadas e preços, permitindo aos usuários explorar as opções disponíveis.
+
+Avaliações de clientes: 
+
+Incluí seções de avaliações onde os clientes podem compartilhar suas experiências, proporcionando confiança e credibilidade aos visitantes.
+
+Responsividade: 
+
+Garantir que a plataforma seja totalmente responsiva, adaptando-se de forma elegante a diferentes dispositivos e tamanhos de tela, proporcionando uma experiência consistente em todos os dispositivos.
+
+A Cafeteria Toque Sedutor é mais do que apenas uma plataforma online, é um convite para uma experiência sensorial única. Desde o aroma do café até a atmosfera acolhedora, nosso objetivo é proporcionar aos clientes uma jornada deliciosa e memorável.
+
+
+<br><b>Contato:</b>
+
+<p>Se você quiser entrar em contato comigo, sinta-se a vontade.</p> 
+
+<a href="https://linkedin.com/in/danielengineer" target="_blank">
+  <img align="center" src="https://img.shields.io/badge/ - LinkedIn-05122A?style=flat&logo=linkedin" alt="linkedin"/>
+</a>
+ <a href="https://instagram.com/danielengineer_" target="_blank">
+ <img align="center" src="https://img.shields.io/badge/ - Instagram-05122A?style=flat&logo=instagram" alt="instagram"/>
+</a>
+ <a href="https://wa.me/77999109489" target="_blank">
+ <img align="center" src="https://img.shields.io/badge/-Whatsapp-05122A?style=flat&logo=whatsapp" alt="whatsapp"/>
+</a>
+<a href="https://www.behance.net/danielengineer_" target="_blank">
+ <img align="center" src="https://img.shields.io/badge/-behance-05122A?style=flat&logo=behance" alt="behance"/>
+</a>
+
--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-// Swiper js
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
-  // grabCursor: true,
   loop: true,
   pagination: {
     el: ".swiper-pagination",
@@ -13,79 +11,71 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-
 // Nav open close
 const body = document.querySelector('body'),
-      navMenu = body.querySelector('.menu-content'),
-      navOpenBtn = body.querySelector('.navOpen-btn'),
-      navCloseBtn = navMenu.querySelector('.navClose-btn');
+  navMenu = body.querySelector('.menu-content'),
+  navOpenBtn = body.querySelector('.navOpen-btn'),
+  navCloseBtn = navMenu.querySelector('.navClose-btn');
 
-if(navMenu && navOpenBtn){
-  navOpenBtn.addEventListener("click", () =>{
+if (navMenu && navOpenBtn) {
+  navOpenBtn.addEventListener("click", () => {
     navMenu.classList.add("open");
     body.style.overflowY = "hidden";
   })
 }
 
-if(navMenu && navCloseBtn){
-  navCloseBtn.addEventListener("click", () =>{
+if (navMenu && navCloseBtn) {
+  navCloseBtn.addEventListener("click", () => {
     navMenu.classList.remove("open");
     body.style.overflowY = "scroll";
   })
 }
 
-// Change header bg color
 window.addEventListener("scroll", () => {
   const scrollY = window.pageYOffset;
 
-  if(scrollY > 5){
+  if (scrollY > 5) {
     document.querySelector("header").classList.add("header-active");
-  }else{
+  } else {
     document.querySelector("header").classList.remove("header-active");
   }
 
-  // Scroll up button
   const scrollUpBtn = document.querySelector('.scrollUp-btn');
 
-  if(scrollY > 250){
+  if (scrollY > 250) {
     scrollUpBtn.classList.add("scrollUpBtn-active");
-  }else{
+  } else {
     scrollUpBtn.classList.remove("scrollUpBtn-active");
   }
-  
-  
-  // Nav link indicator
 
   const sections = document.querySelectorAll('section[id]');
-  sections.forEach(section =>{
+  sections.forEach(section => {
     const sectionHeight = section.offsetHeight,
-          sectionTop = section.offsetTop - 100;
+      sectionTop = section.offsetTop - 100;
 
-          let navId = document.querySelector(`.menu-content a[href='#${section.id}']`);
-          if(scrollY > sectionTop && scrollY <=  sectionTop + sectionHeight){
-            navId.classList.add("active-navlink");           
-          }else{
-            navId.classList.remove("active-navlink");     
-          }
-          
-          navId.addEventListener("click", () => {
-            navMenu.classList.remove("open");
-            body.style.overflowY = "scroll";
-          })
-  })
-})  
-  
-  
-  // Scroll Reveal Animation
-  const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-  })
-  
-  sr.reveal(`.section-title, .section-subtitle, .section-description, .brand-image, .tesitmonial, .newsletter 
-.logo-content, .newsletter-inputBox, .newsletter-mediaIcon, .footer-content, .footer-links`, {interval:100,})
+    let navId = document.querySelector(`.menu-content a[href='#${section.id}']`);
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      navId.classList.add("active-navlink");
+    } else {
+      navId.classList.remove("active-navlink");
+    }
 
-sr.reveal(`.about-imageContent, .menu-items`, {origin: 'left'})
-sr.reveal(`.about-details, .time-table`, {origin: 'right'})
+    navId.addEventListener("click", () => {
+      navMenu.classList.remove("open");
+      body.style.overflowY = "scroll";
+    })
+  })
+})
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+})
+
+sr.reveal(`.section-title, .section-subtitle, .section-description, .brand-image, .tesitmonial, .newsletter 
+.logo-content, .newsletter-inputBox, .newsletter-mediaIcon, .footer-content, .footer-links`, { interval: 100, })
+
+sr.reveal(`.about-imageContent, .menu-items`, { origin: 'left' })
+sr.reveal(`.about-details, .time-table`, { origin: 'right' })
